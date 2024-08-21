49446f4fa591e0862421b002a7d84c83ab2c9a05
jaeyoung99-lee <leejy0715@naver.com>
2024-08-21 17:24:50 +0900

    Make a Navbar // Searchbar // Login button // Logo Image

---
+++ src/component/Navbar.js
@@ -0,0 +1,44 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+  return (
+    <div>
+      <div className="login-button">
+        <FontAwesomeIcon icon={faUser} />
+        <div>로그인</div>
+      </div>
+      <div className="logo">
+        <img
+          width={100}
+          src="https://blog.kakaocdn.net/dn/mPpwY/btrS08qGYmD/HMrVGbYtNNpxynW3i53Zy1/img.png"
+        />
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+        <div className="search">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
